a7540f66823a4a72e8214130b7c09e59fedf42da
jitendrasonata <jitendra.singh@sonata-software.com>
2022-03-13 19:49:56 +0530

    changes in return json data

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -6,7 +6,7 @@ router.post('/registerUser', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
   utils.addUser(username, password);
-  res.json('User is registered successfully');
+  res.json({"message": 'User is registered successfully'});
 });
 
 router.get('/users', (req, res) => {
@@ -20,21 +20,21 @@ router.delete('/delete/:username', (req, res) => {
   if(user && user.length > 0 && user[0].identityProvider) {
     utils.deactivateSession(user[0].identityProvider.idFromProvider);
   }  
-  res.json("User is deleted successfully");
+  res.json({"message": "User is deleted successfully"});
 });
 
 router.post('/login', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
   utils.login(username, password);
-  res.json("User has logged in successfully");
+  res.json({"message": "User has logged in successfully"});
 });
 
 router.post('/logout', (req, res) => {
   const username = req.body.username;
   utils.logout(username);
   utils.deactivateSession(req.authUser.id);
-  res.json("User has logged out successfully");
+  res.json({"message": "User has logged out successfully"});
 });
 
 module.exports = router;
